8739926db462d0a8254408627b0f754422c11b56
Clickadelic <toby.hopp@gmail.com>
2023-11-15 03:25:43 +0100

    Init

---
+++ wp-backend-ad-blocker.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: WordPress Backend AdBlocker
+Plugin URI:  https://bitbucket.org/webdev-hq/dod-motors-admin-customization/src/master/
+Description: Blocks many of the Pro-Ads for plugins in the WordPress backend.
+Version:     1.0.0
+Author:      Tobias Hopp
+Author URI:  https://www.tobias-hopp.de
+License:     GPL2
+License URI: GPL2
+Text Domain: wp-backend-ad-blocker
+Domain Path: /languages
+*/
+
+if(!defined('ABSPATH')) {
+	exit('NaNa nAnA NaNa nAnA NaNa nAnA Batman!');
+}
+
